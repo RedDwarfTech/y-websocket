a5bd7754768ff992de6a0107c2c0b60e44fd9898
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-12 20:17:20 +0800

    feat: add ydoc text get code

--- bin/utils.js
+++ bin/utils.js
@@ -41,6 +41,7 @@ if (typeof persistenceDir === 'string') {
       Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc))
       ydoc.on('update', update => {
         ldb.storeUpdate(docName, update)
+        console.info(persistedYdoc.getText(docName))
         if (persistedYdoc && persistedYdoc.snapshot) {
           const content = Y.decodeSnapshot(persistedYdoc.snapshot)
           handleFileSync(docName, ydoc, content)

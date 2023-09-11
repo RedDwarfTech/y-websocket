afede1ab15b4980934393466092419868c185e43
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-11 23:23:41 +0800

    feat: add template initial handle logic

--- bin/http_server.js
+++ bin/http_server.js
@@ -1,15 +1,38 @@
-const http = require('http')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/y-websocket/file/initial' && req.method === 'GET') {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Initial file requested')
-  }
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello, World!')
-})
+const app = express();
+app.use(bodyParser.json());
 
-const port = 3000
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+app.post('/y-websocket/file/initial', (req, res) => {
+  const docId = req.body.docId;
+  const projectId = req.body.projectId;
+  const initContext = req.body.initContext;
+  console.log(docId, projectId, initContext);
+  init_tpl(docId, projectId, initContext);
+  res.send('请求已成功处理');
+});
+
+app.listen(3000, () => {
+  console.log('服务器已启动');
+});
+
+const init_tpl = (docId, projectId, initContext) => {
+    let docOpt = {
+        guid: docId,
+        collectionid: projectId
+    };
+    const ydoc = new Y.Doc(docOpt);
+    const wsProvider = new WebsocketProvider('ws://localhost:1234', docId, ydoc, { WebSocketPolyfill: require('ws') })
+    wsProvider.on('status', (event) => {
+        if (event.status === 'connected') {
+            if (wsProvider.ws) {
+                if (initContext && initContext.length > 0) {
+                    console.log("write: {}", initContext);
+                    ytext.insert(0, initContext);
+                }
+            }
+        }
+    });
+}
+

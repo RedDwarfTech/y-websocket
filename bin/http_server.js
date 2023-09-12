ecd3388093760d3896096ae19da5de15e15e394c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-12 09:26:28 +0800

    refactor: format issue

--- bin/http_server.js
+++ bin/http_server.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
+const express = require('express')
+const bodyParser = require('body-parser')
+const Y = require('yjs')
+const WebsocketProvider = require('y-websocket')
+const app = express()
+app.use(bodyParser.json())
 
 app.post('/y-websocket/file/initial', (req, res) => {
-  const docId = req.body.doc_id;
-  const projectId = req.body.project_id;
-  const initContext = req.body.file_content;
-  console.log(docId, projectId, initContext);
-  init_tpl(docId, projectId, initContext);
-  res.send('请求已成功处理');
-});
+  const docId = req.body.doc_id
+  const projectId = req.body.project_id
+  const initContext = req.body.file_content
+  console.log(docId, projectId, initContext)
+  initTpl(docId, projectId, initContext)
+  res.send('请求已成功处理')
+})
 
 app.listen(3000, () => {
-  console.log('服务器已启动');
-});
+  console.log('服务器已启动')
+})
 
-const init_tpl = (docId, projectId, initContext) => {
-    let docOpt = {
-        guid: docId,
-        collectionid: projectId
-    };
-    const ydoc = new Y.Doc(docOpt);
-    const wsProvider = new WebsocketProvider('ws://localhost:1234', docId, ydoc, { WebSocketPolyfill: require('ws') })
-    wsProvider.on('status', (event) => {
-        if (event.status === 'connected') {
-            if (wsProvider.ws) {
-                if (initContext && initContext.length > 0) {
-                    console.log("write: {}", initContext);
-                    ytext.insert(0, initContext);
-                }
-            }
+const initTpl = (docId, projectId, initContext) => {
+  let docOpt = {
+    guid: docId,
+    collectionid: projectId
+  }
+  const ydoc = new Y.Doc(docOpt)
+  const ytext = ydoc.getText(docId)
+  const wsProvider = new WebsocketProvider('ws://localhost:1234', docId, ydoc, { WebSocketPolyfill: require('ws') })
+  wsProvider.on('status', (event) => {
+    if (event.status === 'connected') {
+      if (wsProvider.ws) {
+        if (initContext && initContext.length > 0) {
+          console.log('write: {}', initContext)
+          ytext.insert(0, initContext)
         }
-    });
+      }
+    }
+  })
 }
-

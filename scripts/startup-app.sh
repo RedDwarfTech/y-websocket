8808e25b35cab11f79e806586e940d62710501ac
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-12 16:05:24 +0800

    feat: tweak the yjs file storage path

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -2,7 +2,7 @@
 
 export HOST=0.0.0.0
 export PORT=1234
-export YPERSISTENCE=/opt/storage
+export YPERSISTENCE=/opt/data/yjs-storage
 
 nohup ./bin/server.js >> ws.log 2>&1 &
 nohup node ./bin/http_server.js >> http.log 2>&1 &

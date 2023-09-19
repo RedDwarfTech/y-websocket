7cf3f60167e7cd87a3ab76712872376efb513104
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-19 11:05:23 +0800

    refactor: ignore the health check path when doing authorize

--- bin/server.js
+++ bin/server.js
@@ -12,6 +12,7 @@ const setupWSConnection = require('./utils.js').setupWSConnection
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 1234
 const JWT_SIGN_KEY = process.env.JWT_SIGN_KEY || 'key-missing'
+const HEALTHZ_PATH = '/healthz'
 
 const server = http.createServer((request, response) => {
   if (request.url === '/healthz') {
@@ -33,19 +34,21 @@ server.on('upgrade', (request, socket, head) => {
    */
   const handleAuth = ws => {
     const url = new URL(request.url, 'wss://ws.poemhub.top')
-    const token = url.searchParams.get('token')
-    if (!token) {
-      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
-      socket.destroy()
-      return
-    }
-    try {
-      console.log('sign key:' + JWT_SIGN_KEY)
-      jwt.verify(token, JWT_SIGN_KEY)
-      wss.emit('connection', ws, request)
-    } catch (err) {
-      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
-      socket.destroy()
+    if (url.pathname !== HEALTHZ_PATH) {
+      const token = url.searchParams.get('token')
+      if (!token) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+        socket.destroy()
+        return
+      }
+      try {
+        console.log('sign key:' + JWT_SIGN_KEY)
+        jwt.verify(token, JWT_SIGN_KEY)
+        wss.emit('connection', ws, request)
+      } catch (err) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+        socket.destroy()
+      }
     }
   }
   wss.handleUpgrade(request, socket, head, handleAuth)

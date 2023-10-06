34a3c558a64d27e6a40cd06d4315ea8b1a7c3d28
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-07 00:06:57 +0800

    feat: add core dump api

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,8 @@
  */
 const WebSocket = require('ws')
 const http = require('http')
+const fs = require('fs')
+const v8 = require('v8')
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.js').setupWSConnection
 const host = process.env.HOST || '0.0.0.0'
@@ -14,11 +16,24 @@ const server = http.createServer((request, response) => {
   if (request.url === '/healthz') {
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.end('ok')
+  } else if (request.url === '/dump') {
+    memorySnapshot()
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('okay')
   } else {
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.end('okay')
   }
 })
+
+const memorySnapshot = () => {
+  const snapshotStream = v8.getHeapSnapshot()
+  // It's important that the filename end with `.heapsnapshot`,
+  // otherwise Chrome DevTools won't open it.
+  const fileName = `/opt/data/y-websocket-dump/${Date.now()}.heapsnapshot`
+  const fileStream = fs.createWriteStream(fileName)
+  snapshotStream.pipe(fileStream)
+}
 
 wss.on('connection', setupWSConnection)
 

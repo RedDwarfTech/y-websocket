698d40f522d7cbf1049d9485f8190ad5145922ce
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-23 23:21:22 +0800

    feat: add inspect parameters

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -5,6 +5,6 @@ export PORT=1234
 export YPERSISTENCE=/opt/data/yjs-storage
 
 nohup ./bin/server.js >> ws.log 2>&1 &
-nohup node ./bin/http_server.js >> http.log 2>&1 &
+nohup node --inspect=0.0.0.0:9229 ./bin/http_server.js >> http.log 2>&1 &
 
 tail -f ws.log

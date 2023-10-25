f2c6a65417dd0d57e8c85e366701e823cc91e80c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-25 22:29:51 +0800

    feat: add log

--- bin/tex/initial_tpl.js
+++ bin/tex/initial_tpl.js
@@ -8,10 +8,13 @@ exports.initTpl = (docId, projectId, initContext) => {
   }
   const ydoc = new Y.Doc(docOpt)
   const ytext = ydoc.getText(docId)
+  console.log('init context:' + initContext)
   const wsProvider = new WebsocketProvider('ws://localhost:1234/y-websocket/file/initial', docId, ydoc, { WebSocketPolyfill: require('ws') })
   wsProvider.on('status', (event) => {
     if (event.status === 'connected') {
+      console.log('connected')
       if (wsProvider.ws) {
+        console.log('ws')
         if (initContext && initContext.length > 0) {
           ytext.insert(0, initContext)
         }

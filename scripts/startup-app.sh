b91fb55ff3533717f5f07556aeb0ce39a3892ee1
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-25 16:05:39 +0800

    refactor: change the heap size of node

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -3,9 +3,10 @@
 export HOST=0.0.0.0
 export PORT=1234
 export YPERSISTENCE=/opt/data/yjs-storage
-export NODE_OPTIONS="--max-old-space-size=256"
+# export NODE_OPTIONS="--max-old-space-size=256"
 
-nohup node --inspect ./bin/server.js >> ws.log 2>&1 &
+# https://stackoverflow.com/questions/77357320/is-it-possible-to-change-the-node-heap-size
+nohup node --max-old-space-size=256 --inspect ./bin/server.js >> ws.log 2>&1 &
 nohup node --inspect=0.0.0.0:9229 ./bin/http_server.js >> http.log 2>&1 &
 
 tail -f ws.log

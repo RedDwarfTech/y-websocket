0cea1c049b8624e853a3e9b4bee147ddaedd5285
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-26 12:56:37 +0800

    feat: add doc query

--- bin/doc/doc_controller.js
+++ bin/doc/doc_controller.js
@@ -1,14 +1,11 @@
-const persistenceDir = process.env.YPERSISTENCE
 const express = require('express')
 const router = express.Router()
+const docs = require('../utils').docs
 
 router.get('/', async (req, res) => {
   const docId = req.params.docId
-  const LeveldbPersistence = require('y-leveldb').LeveldbPersistence
-  const ldb = new LeveldbPersistence(persistenceDir)
-  const persistedYdoc = await ldb.getYDoc(docId)
-  let text = persistedYdoc.getText(docId)
-  res.send(text)
+  const doc = docs.get(docId)
+  res.send(doc)
 })
 
 module.exports = router

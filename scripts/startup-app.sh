c3e01ad8a59ed1d4b3fe22884b8214e0519ddafb
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-26 11:54:39 +0800

    refactor: tweak

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -6,6 +6,6 @@ export YPERSISTENCE=/opt/data/yjs-storage
 # export NODE_OPTIONS="--max-old-space-size=256"
 
 # https://stackoverflow.com/questions/77357320/is-it-possible-to-change-the-node-heap-size
-nohup node --max-old-space-size=256 --inspect ./bin/server.js >> ws.log 2>&1 &
+nohup node --max-old-space-size=256 --inspect ./bin/server-express.js >> ws.log 2>&1 &
 
 tail -f ws.log

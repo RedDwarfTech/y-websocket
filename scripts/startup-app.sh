6b4ecdaf1bcfa95628f2d0e3a5c8db6946ca03d4
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-27 19:27:27 +0800

    feat: increase the semi memory size

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -6,6 +6,6 @@ export YPERSISTENCE=/opt/data/yjs-storage
 # export NODE_OPTIONS="--max-old-space-size=256"
 
 # https://stackoverflow.com/questions/77357320/is-it-possible-to-change-the-node-heap-size
-nohup node --max-old-space-size=256 --max-semi-space-size=64 ./bin/server-express.js >> ws.log 2>&1 &
+nohup node --max-old-space-size=256 --max-semi-space-size=128 ./bin/server-express.js >> ws.log 2>&1 &
 
 tail -f ws.log

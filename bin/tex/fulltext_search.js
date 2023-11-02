9e91adda7c4ddb3b274c3c9d4ad996b8e2971733
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-11-02 20:21:28 +0800

    refactor: tweak the log to json

--- bin/tex/fulltext_search.js
+++ bin/tex/fulltext_search.js
@@ -14,7 +14,7 @@ const updateFullsearch = async (file) => {
       apiKey: masterKey
     })
     client.index('files').addDocuments([file], option)
-      .then((res) => logger.warn(res + ',file:' + file))
+      .then((res) => logger.warn(res + ',file:' + JSON.stringify(file)))
   } catch (err) {
     logger.error('Failed to sync file index', err)
   }

a6ad14c5b8eb213db885a63f311b6754803339f1
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-11-28 16:47:13 +0800

    feat: tweak the dump file name to more readable

--- bin/profile/profile_controller.js
+++ bin/profile/profile_controller.js
@@ -5,12 +5,12 @@ const v8 = require('v8')
 
 router.get('/dump', (req, res) => {
   const snapshotStream = v8.getHeapSnapshot()
+  const fileAttri = `${now.toISOString().slice(0, 19).replace("T", "_").replace(/:/g, "-")}`
   // It's important that the filename end with `.heapsnapshot`,
   // otherwise Chrome DevTools won't open it.
-  const fileName = `/opt/data/y-websocket-dump/${Date.now()}.heapsnapshot`
+  const fileName = `/opt/data/y-websocket-dump/${fileAttri}.heapsnapshot`
   const fileStream = fs.createWriteStream(fileName)
   snapshotStream.pipe(fileStream)
-
   res.send('ok')
 })
 

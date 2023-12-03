534e07d5e6de83b38d31d0c32f0ee0751a9d8cbc
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-12-04 00:34:31 +0800

    feat: recover the ci

--- bin/server-express.js
+++ bin/server-express.js
@@ -1,37 +1,41 @@
 #!/usr/bin/env node
+
 /**
  * @type {any}
  */
-var yWebSocket = require('ws');
-var http = require('http');
-var wss = new yWebSocket.Server({ noServer: true });
-var setupWSConnection = require('./utils.js').setupWSConnection;
-var host = process.env.HOST || '0.0.0.0';
-var port = process.env.PORT || 1234;
-var app = require('./common/tex_express.js').app;
-var healthRoute = require('./health/health_controller.js');
-var docRoute = require('./doc/doc_controller.js');
-var profileRoute = require('./profile/profile_controller.js');
-var metricsRoute = require('./profile/metrics_controller.js');
-var server = http.createServer(app);
-var express = require('express');
-wss.on('connection', setupWSConnection);
-server.on('upgrade', function (request, socket, head) {
-    // You may check auth of request here..
-    // See https://github.com/websockets/ws#client-authentication
-    /**
-     * @param {WebSocket} ws
-     */
-    var handleAuth = function (ws) {
-        wss.emit('connection', ws, request);
-    };
-    wss.handleUpgrade(request, socket, head, handleAuth);
-});
-app.use(express.json());
-app.use('/health', healthRoute);
-app.use('/doc', docRoute);
-app.use('/profile', profileRoute);
-app.use('/metrics', metricsRoute);
-server.listen(port, host, function () {
-    console.log("running at '".concat(host, "' on port ").concat(port));
-});
+const WebSocket = require('ws')
+const http = require('http')
+const wss = new WebSocket.Server({ noServer: true })
+const setupWSConnection = require('./utils.js').setupWSConnection
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 1234
+const app = require('./common/tex_express.js').app
+const healthRoute = require('./health/health_controller.js')
+const docRoute = require('./doc/doc_controller.js')
+const profileRoute = require('./profile/profile_controller.js')
+const metricsRoute = require('./profile/metrics_controller.js')
+const server = http.createServer(app)
+const express = require('express')
+
+wss.on('connection', setupWSConnection)
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {WebSocket} ws
+   */
+  const handleAuth = (ws) => {
+    wss.emit('connection', ws, request)
+  }
+  wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+app.use(express.json())
+app.use('/health', healthRoute)
+app.use('/doc', docRoute)
+app.use('/profile', profileRoute)
+app.use('/metrics', metricsRoute)
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`)
+})

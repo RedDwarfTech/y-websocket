7f4f7b2674294d5fe2faef0c8e7d4ed544ea79c8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-10-26 13:01:14 +0800

    refactor: tweak the websocket status check

--- bin/utils.js
+++ bin/utils.js
@@ -1,84 +1,86 @@
-const Y = require('yjs')
-const syncProtocol = require('y-protocols/dist/sync.cjs')
-const awarenessProtocol = require('y-protocols/dist/awareness.cjs')
+const Y = require("yjs");
+const syncProtocol = require("y-protocols/dist/sync.cjs");
+const awarenessProtocol = require("y-protocols/dist/awareness.cjs");
 
-const encoding = require('lib0/dist/encoding.cjs')
-const decoding = require('lib0/dist/decoding.cjs')
-const mapWrapper = require('lib0/dist/map.cjs')
-const debounce = require('lodash.debounce')
-const jwt = require('jsonwebtoken')
-var log4js = require('log4js')
-var logger = log4js.getLogger()
-logger.level = 'warn'
-const callbackHandler = require('./callback.js').callbackHandler
-const isCallbackSet = require('./callback.js').isCallbackSet
-const rdfile = require('./rd_file.js')
+const encoding = require("lib0/dist/encoding.cjs");
+const decoding = require("lib0/dist/decoding.cjs");
+const mapWrapper = require("lib0/dist/map.cjs");
+const debounce = require("lodash.debounce");
+const jwt = require("jsonwebtoken");
+var log4js = require("log4js");
+var logger = log4js.getLogger();
+logger.level = "warn";
+const callbackHandler = require("./callback.js").callbackHandler;
+const isCallbackSet = require("./callback.js").isCallbackSet;
+const rdfile = require("./rd_file.js");
 
-const CALLBACK_DEBOUNCE_WAIT = parseInt(process.env.CALLBACK_DEBOUNCE_WAIT) || 2000
-const CALLBACK_DEBOUNCE_MAXWAIT = parseInt(process.env.CALLBACK_DEBOUNCE_MAXWAIT) || 10000
-const JWT_SIGN_KEY = process.env.JWT_SIGN_KEY || 'key-missing'
+const CALLBACK_DEBOUNCE_WAIT =
+  parseInt(process.env.CALLBACK_DEBOUNCE_WAIT) || 2000;
+const CALLBACK_DEBOUNCE_MAXWAIT =
+  parseInt(process.env.CALLBACK_DEBOUNCE_MAXWAIT) || 10000;
+const JWT_SIGN_KEY = process.env.JWT_SIGN_KEY || "key-missing";
 
-const wsReadyStateConnecting = 0
-const wsReadyStateOpen = 1
-const wsReadyStateClosing = 2 // eslint-disable-line
-const wsReadyStateClosed = 3 // eslint-disable-line
+const wsReadyStateConnecting = 0;
+const wsReadyStateOpen = 1;
+const wsReadyStateClosing = 2; // eslint-disable-line
+const wsReadyStateClosed = 3; // eslint-disable-line
 
-const WEBSOCKET_AUTH_FAILED = 4000
-const WEBSOCKET_AUTH_TOKEN_EXPIRE = 4001
+const WEBSOCKET_AUTH_FAILED = 4000;
+const WEBSOCKET_AUTH_TOKEN_EXPIRE = 4001;
 
 // disable gc when using snapshots!
-const gcEnabled = process.env.GC !== 'false' && process.env.GC !== '0'
-const persistenceDir = process.env.YPERSISTENCE
+const gcEnabled = process.env.GC !== "false" && process.env.GC !== "0";
+const persistenceDir = process.env.YPERSISTENCE;
 /**
  * @type {{bindState: function(string,WSSharedDoc):void, writeState:function(string,WSSharedDoc):Promise<any>, provider: any}|null}
  */
-let persistence = null
-if (typeof persistenceDir === 'string') {
-  console.info('Persisting documents to "' + persistenceDir + '"')
+let persistence = null;
+if (typeof persistenceDir === "string") {
+  console.info('Persisting documents to "' + persistenceDir + '"');
   // @ts-ignore
-  const LeveldbPersistence = require('y-leveldb').LeveldbPersistence
-  const ldb = new LeveldbPersistence(persistenceDir)
+  const LeveldbPersistence = require("y-leveldb").LeveldbPersistence;
+  const ldb = new LeveldbPersistence(persistenceDir);
   persistence = {
     provider: ldb,
     bindState: async (docName, ydoc) => {
-      const persistedYdoc = await ldb.getYDoc(docName)
-      const newUpdates = Y.encodeStateAsUpdate(ydoc)
-      ldb.storeUpdate(docName, newUpdates)
-      Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc))
-      ydoc.on('update', update => {
-        ldb.storeUpdate(docName, update)
+      const persistedYdoc = await ldb.getYDoc(docName);
+      const newUpdates = Y.encodeStateAsUpdate(ydoc);
+      ldb.storeUpdate(docName, newUpdates);
+      Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+      ydoc.on("update", (update) => {
+        ldb.storeUpdate(docName, update);
         if (persistedYdoc) {
-          rdfile.throttledFn(docName, ldb)
+          rdfile.throttledFn(docName, ldb);
         }
-      })
+      });
     },
-    writeState: async (docName, ydoc) => { }
-  }
+    writeState: async (docName, ydoc) => {},
+  };
 }
 
 /**
  * @param {{bindState: function(string,WSSharedDoc):void,
  * writeState:function(string,WSSharedDoc):Promise<any>,provider:any}|null} persistence_
  */
-exports.setPersistence = persistence_ => {
-  persistence = persistence_
-}
+exports.setPersistence = (persistence_) => {
+  persistence = persistence_;
+};
 
 /**
  * @return {null|{bindState: function(string,WSSharedDoc):void,
-  * writeState:function(string,WSSharedDoc):Promise<any>}|null} used persistence layer
-  */
-exports.getPersistence = () => persistence
+ * writeState:function(string,WSSharedDoc):Promise<any>}|null} used persistence layer
+ */
+exports.getPersistence = () => persistence;
 
 /**
  * @type {Map<string,WSSharedDoc>}
  */
-const docs = new Map()
+const docs = new Map();
 // exporting docs so that others can use it
-exports.docs = docs
+exports.docs = docs;
 
-const messageSync = 0
-const messageAwareness = 1
+const messageSync = 0;
+const messageAwareness = 1;
 // const messageAuth = 2
 
 /**
@@ -87,60 +89,70 @@ const messageAwareness = 1
  * @param {WSSharedDoc} doc
  */
 const updateHandler = (update, origin, doc) => {
-  const encoder = encoding.createEncoder()
-  encoding.writeVarUint(encoder, messageSync)
-  syncProtocol.writeUpdate(encoder, update)
-  const message = encoding.toUint8Array(encoder)
-  doc.conns.forEach((_, conn) => send(doc, conn, message))
-}
+  const encoder = encoding.createEncoder();
+  encoding.writeVarUint(encoder, messageSync);
+  syncProtocol.writeUpdate(encoder, update);
+  const message = encoding.toUint8Array(encoder);
+  doc.conns.forEach((_, conn) => send(doc, conn, message));
+};
 
 class WSSharedDoc extends Y.Doc {
   /**
    * @param {string} name
    */
-  constructor (name) {
-    super({ gc: gcEnabled })
-    this.name = name
+  constructor(name) {
+    super({ gc: gcEnabled });
+    this.name = name;
     /**
      * Maps from conn to set of controlled user ids. Delete all user ids from awareness when this conn is closed
      * @type {Map<Object, Set<number>>}
      */
-    this.conns = new Map()
+    this.conns = new Map();
     /**
      * @type {awarenessProtocol.Awareness}
      */
-    this.awareness = new awarenessProtocol.Awareness(this)
-    this.awareness.setLocalState(null)
+    this.awareness = new awarenessProtocol.Awareness(this);
+    this.awareness.setLocalState(null);
     /**
      * @param {{ added: Array<number>, updated: Array<number>, removed: Array<number> }} changes
      * @param {Object | null} conn Origin is the connection that made the change
      */
     const awarenessChangeHandler = ({ added, updated, removed }, conn) => {
-      const changedClients = added.concat(updated, removed)
+      const changedClients = added.concat(updated, removed);
       if (conn !== null) {
-        const connControlledIDs = /** @type {Set<number>} */ (this.conns.get(conn))
+        const connControlledIDs = /** @type {Set<number>} */ (
+          this.conns.get(conn)
+        );
         if (connControlledIDs !== undefined) {
-          added.forEach(clientID => { connControlledIDs.add(clientID) })
-          removed.forEach(clientID => { connControlledIDs.delete(clientID) })
+          added.forEach((clientID) => {
+            connControlledIDs.add(clientID);
+          });
+          removed.forEach((clientID) => {
+            connControlledIDs.delete(clientID);
+          });
         }
       }
       // broadcast awareness update
-      const encoder = encoding.createEncoder()
-      encoding.writeVarUint(encoder, messageAwareness)
-      encoding.writeVarUint8Array(encoder, awarenessProtocol.encodeAwarenessUpdate(this.awareness, changedClients))
-      const buff = encoding.toUint8Array(encoder)
+      const encoder = encoding.createEncoder();
+      encoding.writeVarUint(encoder, messageAwareness);
+      encoding.writeVarUint8Array(
+        encoder,
+        awarenessProtocol.encodeAwarenessUpdate(this.awareness, changedClients)
+      );
+      const buff = encoding.toUint8Array(encoder);
       this.conns.forEach((_, c) => {
-        send(this, c, buff)
-      })
-    }
-    this.awareness.on('update', awarenessChangeHandler)
-    this.on('update', updateHandler)
+        send(this, c, buff);
+      });
+    };
+    this.awareness.on("update", awarenessChangeHandler);
+    this.on("update", updateHandler);
     if (isCallbackSet) {
-      this.on('update', debounce(
-        callbackHandler,
-        CALLBACK_DEBOUNCE_WAIT,
-        { maxWait: CALLBACK_DEBOUNCE_MAXWAIT }
-      ))
+      this.on(
+        "update",
+        debounce(callbackHandler, CALLBACK_DEBOUNCE_WAIT, {
+          maxWait: CALLBACK_DEBOUNCE_MAXWAIT,
+        })
+      );
     }
   }
 }
@@ -152,17 +164,18 @@ class WSSharedDoc extends Y.Doc {
  * @param {boolean} gc - whether to allow gc on the doc (applies only when created)
  * @return {WSSharedDoc}
  */
-const getYDoc = (docname, gc = true) => mapWrapper.setIfUndefined(docs, docname, () => {
-  const doc = new WSSharedDoc(docname)
-  doc.gc = gc
-  if (persistence !== null) {
-    persistence.bindState(docname, doc)
-  }
-  docs.set(docname, doc)
-  return doc
-})
+const getYDoc = (docname, gc = true) =>
+  mapWrapper.setIfUndefined(docs, docname, () => {
+    const doc = new WSSharedDoc(docname);
+    doc.gc = gc;
+    if (persistence !== null) {
+      persistence.bindState(docname, doc);
+    }
+    docs.set(docname, doc);
+    return doc;
+  });
 
-exports.getYDoc = getYDoc
+exports.getYDoc = getYDoc;
 
 /**
  * @param {any} conn
@@ -171,31 +184,35 @@ exports.getYDoc = getYDoc
  */
 const messageListener = (conn, doc, message) => {
   try {
-    const encoder = encoding.createEncoder()
-    const decoder = decoding.createDecoder(message)
-    const messageType = decoding.readVarUint(decoder)
+    const encoder = encoding.createEncoder();
+    const decoder = decoding.createDecoder(message);
+    const messageType = decoding.readVarUint(decoder);
     switch (messageType) {
       case messageSync:
-        encoding.writeVarUint(encoder, messageSync)
-        syncProtocol.readSyncMessage(decoder, encoder, doc, conn)
+        encoding.writeVarUint(encoder, messageSync);
+        syncProtocol.readSyncMessage(decoder, encoder, doc, conn);
 
         // If the `encoder` only contains the type of reply message and no
         // message, there is no need to send the message. When `encoder` only
         // contains the type of reply, its length is 1.
         if (encoding.length(encoder) > 1) {
-          send(doc, conn, encoding.toUint8Array(encoder))
+          send(doc, conn, encoding.toUint8Array(encoder));
         }
-        break
+        break;
       case messageAwareness: {
-        awarenessProtocol.applyAwarenessUpdate(doc.awareness, decoding.readVarUint8Array(decoder), conn)
-        break
+        awarenessProtocol.applyAwarenessUpdate(
+          doc.awareness,
+          decoding.readVarUint8Array(decoder),
+          conn
+        );
+        break;
       }
     }
   } catch (err) {
-    logger.error('message listener error,' + err)
-    doc.emit('error', [err])
+    logger.error("message listener error," + err);
+    doc.emit("error", [err]);
   }
-}
+};
 
 /**
  * @param {WSSharedDoc} doc
@@ -207,126 +224,165 @@ const closeConn = (doc, conn) => {
      * @type {Set<number>}
      */
     // @ts-ignore
-    const controlledIds = doc.conns.get(conn)
-    doc.conns.delete(conn)
-    awarenessProtocol.removeAwarenessStates(doc.awareness, Array.from(controlledIds), null)
+    const controlledIds = doc.conns.get(conn);
+    doc.conns.delete(conn);
+    awarenessProtocol.removeAwarenessStates(
+      doc.awareness,
+      Array.from(controlledIds),
+      null
+    );
     if (doc.conns.size === 0 && persistence !== null) {
       // if persisted, we store state and destroy ydocument
       persistence.writeState(doc.name, doc).then(() => {
-        doc = doc.destroy()
-      })
-      docs.delete(doc.name)
-      doc.name = null
+        doc = doc.destroy();
+      });
+      docs.delete(doc.name);
+      doc.name = null;
     }
   }
-  conn.close(4000)
-  conn = null
-}
+  conn.close(4000);
+  conn = null;
+};
 
 /**
  * @param {WSSharedDoc} doc
-* @param {WebSocket} conn
+ * @param {WebSocket} conn
  * @param {Uint8Array} m
  */
 const send = (doc, conn, m) => {
-  if (conn.readyState !== wsReadyStateConnecting && conn.readyState !== wsReadyStateOpen) {
-    logger.warn('connection state is not open, doc:' + doc.name)
-    closeConn(doc, conn)
-  }
   try {
-    conn.send(m)
+    if (conn.readyState === wsReadyStateOpen) {
+      conn.send(m);
+    } else {
+      logger.warn("connection state is not open, doc:" + doc.name);
+      closeConn(doc, conn);
+    }
   } catch (e) {
-    const decoder = new TextDecoder('utf-8')
-    const text = decoder.decode(m)
-    logger.error('send message facing error,text:' + text, e)
-    closeConn(doc, conn)
+    const decoder = new TextDecoder("utf-8");
+    const text = decoder.decode(m);
+    logger.error("send message facing error,text:" + text, e);
+    closeConn(doc, conn);
   }
-}
+};
 
-const pingTimeout = 20000
+const pingTimeout = 20000;
 
 /**
-* @param {WebSocket} conn
-* @param {IncomingMessage} request
-*/
+ * @param {WebSocket} conn
+ * @param {IncomingMessage} request
+ */
 const handleAuth = (request, conn) => {
-  const url = new URL(request.url, 'wss://ws.poemhub.top')
-  if (request.url !== '/healthz' && request.headers.host !== 'localhost:1234') {
+  const url = new URL(request.url, "wss://ws.poemhub.top");
+  if (request.url !== "/healthz" && request.headers.host !== "localhost:1234") {
     // https://self-issued.info/docs/draft-ietf-oauth-v2-bearer.html#query-param
-    const token = url.searchParams.get('access_token')
-    const src = url.searchParams.get('from')
+    const token = url.searchParams.get("access_token");
+    const src = url.searchParams.get("from");
     try {
-      jwt.verify(token, JWT_SIGN_KEY)
+      jwt.verify(token, JWT_SIGN_KEY);
     } catch (err) {
       switch (err.name) {
-        case 'TokenExpiredError':
-          logger.warn('token expired:' + err + ', request url:' + request.url + ',token:' + token + ',host:' + request.headers.host)
-          conn.close(WEBSOCKET_AUTH_TOKEN_EXPIRE)
-          break
-        case 'JsonWebTokenError':
-          logger.error('json web token facing error:' + err + ', request url:' + request.url + ',token:' + token + ',src=' + src)
-          conn.close(WEBSOCKET_AUTH_FAILED)
-          break
+        case "TokenExpiredError":
+          logger.warn(
+            "token expired:" +
+              err +
+              ", request url:" +
+              request.url +
+              ",token:" +
+              token +
+              ",host:" +
+              request.headers.host
+          );
+          conn.close(WEBSOCKET_AUTH_TOKEN_EXPIRE);
+          break;
+        case "JsonWebTokenError":
+          logger.error(
+            "json web token facing error:" +
+              err +
+              ", request url:" +
+              request.url +
+              ",token:" +
+              token +
+              ",src=" +
+              src
+          );
+          conn.close(WEBSOCKET_AUTH_FAILED);
+          break;
       }
     }
   }
-}
+};
 
 /**
  * @param {WebSocket} conn
  * @param {IncomingMessage} req
  * @param {any} opts
  */
-exports.setupWSConnection = (conn, req, { docName = req.url.slice(1).split('?')[0], gc = true } = {}) => {
-  handleAuth(req, conn)
-  conn.binaryType = 'arraybuffer'
+exports.setupWSConnection = (
+  conn,
+  req,
+  { docName = req.url.slice(1).split("?")[0], gc = true } = {}
+) => {
+  handleAuth(req, conn);
+  conn.binaryType = "arraybuffer";
   // get doc, initialize if it does not exist yet
-  const doc = getYDoc(docName, gc)
-  doc.conns.set(conn, new Set())
+  const doc = getYDoc(docName, gc);
+  doc.conns.set(conn, new Set());
   // listen and reply to events
-  conn.on('message', /** @param {ArrayBuffer} message */ message => messageListener(conn, doc, new Uint8Array(message)))
+  conn.on(
+    "message",
+    /** @param {ArrayBuffer} message */ (message) =>
+      messageListener(conn, doc, new Uint8Array(message))
+  );
   // Check if connection is still alive
-  let pongReceived = true
+  let pongReceived = true;
   const pingInterval = setInterval(() => {
     if (!pongReceived) {
       if (doc.conns.has(conn)) {
-        logger.warn('close connection pong, pong:' + pongReceived + ',docName:' + docName)
-        closeConn(doc, conn)
+        logger.warn(
+          "close connection pong, pong:" + pongReceived + ",docName:" + docName
+        );
+        closeConn(doc, conn);
       }
-      clearInterval(pingInterval)
+      clearInterval(pingInterval);
     } else if (doc.conns.has(conn)) {
-      pongReceived = false
+      pongReceived = false;
       try {
-        conn.ping()
+        conn.ping();
       } catch (e) {
-        logger.error('close connection when ping,' + e + ',docName:' + docName)
-        closeConn(doc, conn)
-        clearInterval(pingInterval)
+        logger.error("close connection when ping," + e + ",docName:" + docName);
+        closeConn(doc, conn);
+        clearInterval(pingInterval);
       }
     }
-  }, pingTimeout)
-  conn.on('close', (e) => {
-    logger.warn('trigger close event, the doc:' + docName, e)
-    closeConn(doc, conn)
-    clearInterval(pingInterval)
-  })
-  conn.on('pong', () => {
-    pongReceived = true
-  })
+  }, pingTimeout);
+  conn.on("close", (e) => {
+    logger.warn("trigger close event, the doc:" + docName, e);
+    closeConn(doc, conn);
+    clearInterval(pingInterval);
+  });
+  conn.on("pong", () => {
+    pongReceived = true;
+  });
   // put the following in a variables in a block so the interval handlers don't keep in in
   // scope
   {
     // send sync step 1
-    const encoder = encoding.createEncoder()
-    encoding.writeVarUint(encoder, messageSync)
-    syncProtocol.writeSyncStep1(encoder, doc)
-    send(doc, conn, encoding.toUint8Array(encoder))
-    const awarenessStates = doc.awareness.getStates()
+    const encoder = encoding.createEncoder();
+    encoding.writeVarUint(encoder, messageSync);
+    syncProtocol.writeSyncStep1(encoder, doc);
+    send(doc, conn, encoding.toUint8Array(encoder));
+    const awarenessStates = doc.awareness.getStates();
     if (awarenessStates.size > 0) {
-      const encoder = encoding.createEncoder()
-      encoding.writeVarUint(encoder, messageAwareness)
-      encoding.writeVarUint8Array(encoder, awarenessProtocol.encodeAwarenessUpdate(doc.awareness, Array.from(awarenessStates.keys())))
-      send(doc, conn, encoding.toUint8Array(encoder))
+      const encoder = encoding.createEncoder();
+      encoding.writeVarUint(encoder, messageAwareness);
+      encoding.writeVarUint8Array(
+        encoder,
+        awarenessProtocol.encodeAwarenessUpdate(
+          doc.awareness,
+          Array.from(awarenessStates.keys())
+        )
+      );
+      send(doc, conn, encoding.toUint8Array(encoder));
     }
   }
-}
+};
